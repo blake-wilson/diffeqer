843601786232f28c7a7237b3636847589ac9793d
Blake Wilson <bwilson025@gmail.com>
2015-06-05 00:08:23 -0500

    Use range to specify the length of the interval starting at initial time

--- html/app.js
+++ html/app.js
@@ -22,11 +22,12 @@ function requestUpdate() {
 	}
 	xhr.open("POST", HOST, true);
 
+	tinit = Number(document.getElementById("tinit").value)
 	var request = {
 		timestep: Number(document.getElementById("timestep").value) * 0.001,
-		initial_time: Number(document.getElementById("tinit").value),
+		initial_time: tinit,
 		initial_value: Number(document.getElementById("yinit").value),
-		final_time: Number(document.getElementById("range").value),
+		final_time: Number(document.getElementById("range").value) + tinit,
 
 		method: document.getElementById("method").value,
 		expression: document.getElementById("expression").value
